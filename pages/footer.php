11570a2f2785894b8158f183713ff8a5b582b3e7
RokhayaJOhnson <97117442+RokhayaJOhnson@users.noreply.github.com>
2022-01-06 18:34:10 +0000

    Update footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<title></title>
 </head>
 <body>
 
 	<footer>
-		
+		abc
 	</footer>
 
 </body>
