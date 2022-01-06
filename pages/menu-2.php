d6a31fde918613f7d8093389bbef4d69ede152a4
RokhayaJOhnson <97117442+RokhayaJOhnson@users.noreply.github.com>
2022-01-06 18:30:21 +0000

    Update menu-2.php

--- pages/menu-2.php
+++ pages/menu-2.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="../styles/menu.css">
+	<link rel="stylesheet" type="text/css" href="../styles/footer.css">
 </head>
 <body>
 	<header>
