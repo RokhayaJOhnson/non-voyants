de60a92e6789f07720cc60db906d01e277ddff77
Rokhaya <rokhayajo@gmail.com>
2022-01-10 22:54:45 +0000

    modif footer

--- pages/footer.php
+++ pages/footer.php
@@ -12,19 +12,3 @@
 
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<title></title>
-</head>
-<body>
-
-	<footer>
-		
-	</footer>
-
-</body>
-</html>
